476c9e27c7519dd19a84bc122fc61f08745278a5
mamang444 <komangyandy@gmail.com>
2023-01-17 15:19:46 +0800

    update tampilan

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mamang/login_interface/login.dart';
-import 'package:mamang/views/loginviews.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 void main() async {
